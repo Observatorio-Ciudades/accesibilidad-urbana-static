87d73a165712d4159d8c17be7948be6fb0e9f9b5
darredondort <diegoarredondo89@gmail.com>
2020-12-04 02:11:11 -0600

    render hexpaths pruebas

--- src/main.js
+++ src/main.js
@@ -424,6 +424,52 @@ function init() {
         // map.setPaintProperty(layerId, 'fill-color', 'rgba(255,0,0,255)');
         map.setPaintProperty(layerId, 'fill-opacity', config.fillOpacity);
     }
+
+    // function renderHexPaths(map, hexagons) {
+  
+    //     // Transform the current hexagon map into a GeoJSON object
+    //     const geojson = geojson2h3.h3SetToFeatureCollection(
+    //       Object.keys(hexagons),
+    //       hex => ({value: hexagons[hex]})
+    //     );
+        
+    //     const sourceId = 'h3-hexes';
+    //     const layerId = `${sourceId}-layer`;
+    //     let source = map.getSource(sourceId);
+        
+    //     // Add the source and layer if we haven't created them yet
+    //     if (!source) {
+    //       map.addSource(sourceId, {
+    //         type: 'geojson',
+    //         data: geojson
+    //       });
+    //       map.addLayer({
+    //         id: layerId,
+    //         source: sourceId,
+    //         type: 'fill',
+    //         interactive: false,
+    //         paint: {
+    //           'fill-outline-color': 'rgba(0,0,0,0)',
+    //         }
+    //       });
+    //       source = map.getSource(sourceId);
+    //     }
+      
+    //     // Update the geojson data
+    //     source.setData(geojson);
+        
+    //     // Update the layer paint properties, using the current config values
+    //     map.setPaintProperty(layerId, 'fill-color', {
+    //       property: 'value',
+    //       stops: [
+    //         [0, config.colorScale[0]],
+    //         [0.5, config.colorScale[1]],
+    //         [1, config.colorScale[2]]
+    //       ]
+    //     });
+        
+    //     map.setPaintProperty(layerId, 'fill-opacity', config.fillOpacity);
+    //   }
   
   
 
